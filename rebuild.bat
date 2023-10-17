55f612e1fa1e0cdc8932c1ef6f81314663aa08f6
Rowan Barr <rbarr@tcd.ie>
2023-10-17 21:29:11 +0100

    video fiex

--- rebuild.bat
+++ rebuild.bat
@@ -3,6 +3,6 @@ call rm_consumer.bat
 call rm_broker.bat
 docker build -t stream_network .
 
-REM docker network create -d bridge --subnet 172.20.0.0/16 stream_network
+REM docker network create -d bridge --subnet 172.22.0.0/16 streaming_network
 
 REM run order, [rebuild.bat, run_containers.bat]
